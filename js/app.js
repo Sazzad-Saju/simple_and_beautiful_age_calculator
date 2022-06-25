a08e4e80820d3aa8adf55f19d0d308d5b996af49
Sazzad-Saju <sazzadsaju17@gmail.com>
2022-06-25 21:07:53 +0600

    part 03: design done

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 function calculate() {
-    console.log("I'm Here!")
+    dycalendar.draw({
+        target: '#styleCalendar',
+        year: 1994,
+        month: 10,
+        date: 18
+    })
 }
+
+$(function() {
+    $("#datepicker").datepicker({
+        format: "dd-mm-yyyy",
+        clearBtn: true,
+        autoclose: true,
+        endDate: '+0d',
+    })
+})
